a217259a5d660db2624bbdadc06673281e76fa8c
glenclayton <35498386+glenclayton@users.noreply.github.com>
2024-12-01 20:10:04 +0000

    part one passed

--- day01.py
+++ day01.py
@@ -6,5 +6,20 @@ def distance(a,b):
         sum = sum + abs(i-j)
     return sum
 
+def readfile():
+    text_file = open("day01_input.txt", "r")
+    lines = text_file.readlines()
+    a = []
+    b = []
+    for line in lines:
+        sl = line.split()
+        a.append(int(sl[0]))
+        b.append(int(sl[1]))
+    return a,b
+
+first,second = readfile()
+print(first)
+dis = distance(first,second)
+print(f"The distance is {dis}")
 
         
