4c6d3630c8f9f7e6666a8d20b6d3facf968cce5e
glenclayton <35498386+glenclayton@users.noreply.github.com>
2024-12-02 19:23:24 +0000

    unit test passes

--- day02.py
+++ day02.py
@@ -1,2 +1,15 @@
 def is_safe(row):
-    return False
+    diffs = []
+    for i in range(len(row)-1):
+        a = row[i]
+        b = row[i+1]
+        diffs.append(b-a)
+    max_diff = max(diffs)
+    min_diff = min(diffs)
+    rv = False
+    if ((max_diff<4) and (min_diff>0)) or ((min_diff>-4) and max_diff<0):
+        rv = True
+    return rv
+    
+
+
--- test_day02.py
+++ test_day02.py
@@ -3,3 +3,13 @@ from day02 import is_safe
 def test_is_safe():
     row1 = [7,6,4,2,1]
     assert is_safe(row1) == True
+    row2 = [1,2,7,8,9]
+    assert is_safe(row2) == False
+    row3 = [9,7,6,2,1]
+    assert is_safe(row3) == False
+    row4 = [1,3,2,4,5]
+    assert is_safe(row4) == False
+    row5 = [8,6,4,4,1]
+    assert is_safe(row5) == False
+    row6 = [1,3,6,7,9]
+    assert is_safe(row6) == True
