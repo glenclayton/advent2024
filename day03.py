0785acd273bc286f4d8c454408dffe4d78ac0f5a
glenclayton <35498386+glenclayton@users.noreply.github.com>
2024-12-03 10:19:40 +0000

    temp

--- day03.py
+++ day03.py
@@ -1,3 +1,18 @@
+import re
+
+
+
+def extract_tuple(row, c):
+    regex = r"^(\d+),(\d+)$"
+    idx = row.index('mul(',c)
+    idy = row.index(')',idx+4)
+    ts = row[idx+4:idy]
+    matches = re.finditer(regex,ts,re.MULTILINE)
+    
+
+
 def extract_mul(row):
+    c = 0
     return []
+
 
