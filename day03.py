14af4997df614fd667077c379c15af8a39f805d7
glenclayton <35498386+glenclayton@users.noreply.github.com>
2024-12-03 17:16:41 +0000

    unit test passes

--- day03.py
+++ day03.py
@@ -6,20 +6,29 @@ def extract_tuple(row, c):
     regex = r"^(\d+),(\d+)$"
     idx = row.find('mul(',c)
     idy = row.find(')',idx+4)
-    if idx<0 or idy<0 :
-        return (0,0)
+    if idx<0 or idy<0:
+        return None,-1
     ts = row[idx+4:idy]
     thematch = re.match(regex,ts)
     if thematch != None:
         a = thematch.group(1)
         b = thematch.group(2)
-        return (int(a),int(b))
-    return (0,0)
+        return (int(a),int(b)),idy
+    return (0,0),idx+4
     
 
 
 def extract_mul(row):
     c = 0
-    return []
+    i = 0
+    while True:
+        t,i = extract_tuple(row,i)
+        if t == None:
+            break
+        c = c+t[0]*t[1]
+        print(f"{t} found, next pos {i}")
+    return c
+    
+    
 
 
--- test_day03.py
+++ test_day03.py
@@ -2,26 +2,28 @@ from day03 import extract_mul,extract_tuple
 
 def test_extract_tuple():
     row='xmul(2,3)y'
-    rv = extract_tuple(row,0)
+    rv,i = extract_tuple(row,0)
     assert rv == (2,3)
+    assert i == 8
     row='xmul(2 ,3)y'
-    rv = extract_tuple(row,0)
+    rv,i = extract_tuple(row,0)
     assert rv == (0,0)
     row='xmul(2,3y'
-    rv = extract_tuple(row,0)
-    assert rv == (0,0)
+    rv,i = extract_tuple(row,0)
+    assert rv == None
     row='xmul(2,3x)y'
-    rv = extract_tuple(row,0)
+    rv,i = extract_tuple(row,0)
     assert rv == (0,0)
 
 
 def test_extract_mul():
     data = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
     rv = extract_mul(data)
-    assert len(rv) == 4
-    assert rv[0] == (2,4)
-    assert rv[1] == (5,5)
-    assert rv[2] == (11,8)
-    assert rv[3] == (8,5)
+    assert rv == 161
+ #   assert len(rv) == 4
+ #   assert rv[0] == (2,4)
+ #   assert rv[1] == (5,5)
+ #   assert rv[2] == (11,8)
+ #   assert rv[3] == (8,5)
 
 
